217b53b545bea03a320b2755e8934490d43e6594
Ytr <132002974+yusuftrg13@users.noreply.github.com>
2023-12-29 19:55:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using OBJECTMETOTS;
+
+Student student1 = new Student("John", "Business", 2.7);
+Student student2 = new Student("Elissa", "Psychology", 3.5);
+
+Console.WriteLine(student1.HasCheck());
+Console.WriteLine(student1.Name + student1.Departmant);
+Console.WriteLine();
+Console.ReadLine();
+
---
+++ Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBJECTMETOTS
+{
+    internal class Student
+    {
+        //----Object Methots -----
+        public string Name;
+        public string Departmant;
+        public double gpa;
+
+
+        public Student(string aName, string aDepartmant, double agpa)
+        {
+            Name = aName;
+            Departmant = aDepartmant;
+            gpa = agpa;
+        }
+
+
+        public bool HasCheck()
+        {
+            if (gpa < 2.5)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+
+    }
+}
